ecfa078c62f05a358acb7b819eca2d9df513ea3d
shantanu <shantanu171@gmail.com>
2023-03-22 19:14:12 +0530

    added VPC.tf

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Terraform AWS EKS
 
-The tfstate file for the aws-eks project is stored locally 
+The tfstate file for the aws-eks project is stored locally.
+
+The VPC that the EKS cluster is going to be created in, reqiures that the VPC has support for DNS.  
---
+++ vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "eks_vpc" {
+  # CIDR block for the VPC.
+  cidr_block = "10.0.0.0/20"
+
+  # Making the instances in this VPC shared on a host. (Dedicated hosts will incure additional charges from AWS) 
+  instance_tenancy = "default"
+
+  # EKS requires the VPC to have DNS support enabled.
+  enable_dns_support = true
+
+  # EKS requires the VPC to have DNS Hostnames enabled.
+  enable_dns_hostnames = true
+
+  # Disabling the IPv6 for the VPC
+  assign_generated_ipv6_cidr_block = false
+}
