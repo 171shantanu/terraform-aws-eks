3eb12acea9423843fc01dab5d371150733ae6cf4
shantanu <shantanu171@gmail.com>
2023-03-22 23:21:30 +0530

    added subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,55 @@
+#Adding public subnet 1
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/28"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                      = "public-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+#Adding public subnet 2
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/28"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                      = "public-us-east-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+#Adding private subnet 1
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.3.0/28"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                            = "private-us-east-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+#Adding private subnet 2
+resource "aws_subnet" "private_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.4.0/28"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                            = "private-us-east-1b"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
