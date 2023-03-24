36769aeca1c750c4b447df3fb10f6f8d203fedb1
shantanu <shantanu171@gmail.com>
2023-03-24 18:12:23 +0530

    added private route tables

--- route-tables.tf
+++ route-tables.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "public_1" {
   vpc_id = aws_vpc.eks_vpc.id
 
   tags = {
-    "Name" = "${local.name_suffix}-EKS Public Route Table"
+    "Name" = "${local.name_suffix}-EKS-Public-Route-Table"
   }
 }
 
@@ -14,4 +14,24 @@ resource "aws_route" "public_route_1" {
   destination_cidr_block = var.public_route
   gateway_id             = aws_internet_gateway.eks_igw.id
   depends_on             = [aws_internet_gateway.eks_igw]
+}
+
+# Route table for privatve subnet 1
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    "Name" = "${local.name_suffix}-EKS-Private-Route-Table-1"
+  }
+}
+
+# Route table for privatve subnet 1
+
+resource "aws_route_table" "private_2" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    "Name" = "${local.name_suffix}-EKS-Private-Route-Table-2"
+  }
 }
